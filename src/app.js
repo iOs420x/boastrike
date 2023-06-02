9d1dca92f90a8666589c18cd717bb4b29002fa6c
iOs420x <127722637+iOs420x@users.noreply.github.com>
2023-06-02 17:04:31 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const bodyParse = require ("body-parser");
 
 
 // Settings
-app.set('port', 5397);
+app.set('port', 5398);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
